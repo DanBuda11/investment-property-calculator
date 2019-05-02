c90d232a548d43d015b7e8324e17a90388a79565
Dan Buda <danbuda@gmail.com>
2019-05-02 12:49:43 -0500

    service worker another attempt

--- src/sw.js
+++ src/sw.js
@@ -1,58 +1,26 @@
-const CACHE_NAME = 'investment-calc-v1';
-const urlsToCache = ['/', '/css/main.css', '/js/index.js'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll(['/', '/css/main.css', 'js/index.js']);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response !== undefined) {
+        return response;
+      } else {
+        return fetch(event.request).then(function(response) {
+          let responseClone = response.clone();
+
+          caches.open('v1').then(function(cache) {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        });
+      }
+    })
+  );
 });
-
-// self.addeListener('install', function(e) {
-//   e.waitUntil(
-//     caches.open(CACHE_NAME).then(function(cache) {
-//       console.log('Opened cache');
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-// });
-
-// self.addeListener('fetch', function(e) {
-//   e.respondWith(fromCache(e.req));
-
-//   e.waitUntil(update(e.req).then(refresh));
-// });
-
-// function fromCache(req) {
-//   return caches.open(CACHE).then(function(cache) {
-//     return cache.match(req);
-//   });
-// }
-
-// function update(req) {
-//   return caches.open(CACHE).then(function(cache) {
-//     return fetch(req).then(function(res) {
-//       return cache.put(req, res.clone()).then(function() {
-//         return res;
-//       });
-//     });
-//   });
-// }
-
-// function refresh(res) {
-//   return self.clients.matchAll().then(function(clients) {
-//     clients.forEach(function(client) {
-//       const message = {
-//         type: 'refresh',
-//         url: res.url,
-//         eTag: res.headers.get('ETag'),
-//       };
-//       client.postMessage(JSON.stringify(message));
-//     });
-//   });
-// }
