afab0b55cedfd4a37ff1584e38aacf4eb7e21630
Dan Buda <danbuda@gmail.com>
2019-05-02 11:47:55 -0500

    service worker attempt #2

--- src/sw.js
+++ src/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'investment-calc-v1';
+const CACHE = 'investment-calc-v1';
 const urlsToCache = ['/', '/css/main.css', '/js/index.js'];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
+self.addeListener('install', function(e) {
+  e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       console.log('Opened cache');
       return cache.addAll(urlsToCache);
@@ -10,13 +10,37 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
+self.addeListener('fetch', function(e) {
+  e.respondWith(fromCache(e.req));
+
+  e.waitUntil(update(e.req).then(refresh));
 });
+
+function fromCache(req) {
+  return caches.open(CACHE).then(function(cache) {
+    return cache.match(req);
+  });
+}
+
+function update(req) {
+  return caches.open(CACHE).then(function(cache) {
+    return fetch(req).then(function(res) {
+      return cache.put(req, res.clone()).then(function() {
+        return res;
+      });
+    });
+  });
+}
+
+function refresh(res) {
+  return self.clients.matchAll().then(function(clients) {
+    clients.forEach(function(client) {
+      const message = {
+        type: 'refresh',
+        url: res.url,
+        eTag: res.headers.get('ETag'),
+      };
+      client.postMessage(JSON.stringify(message));
+    });
+  });
+}
